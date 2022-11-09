e261016e3f97ae50bcfc714802ad95fa90c5a62f
Danya <danil.volkov.235@gmail.com>
2022-11-09 23:01:26 +0200

    Добавил комментарии

--- Volkov_HW_36_System/Volkov_HW_36_System/main.cpp
+++ Volkov_HW_36_System/Volkov_HW_36_System/main.cpp
@@ -36,50 +36,50 @@ BOOL CALLBACK DlgProc(HWND hWnd, UINT message, WPARAM wp, LPARAM lp)
 
     }
     return TRUE;
-    case WM_COMMAND:
+    case WM_COMMAND: 
     {
-        if (wp == IDC_BUTTON1) {
+        if (wp == IDC_BUTTON1) { // если нажата кнопка старт
             float summ = 0;
             int index = SendMessage(hCombo, CB_GETCURSEL, 0, 0);
-            int number = min + rand() % (max - min + 1);
-            SendMessage(hList, LB_RESETCONTENT, 0, 0);
-            while (arr.size() <= number)
+            int number = min + rand() % (max - min + 1); // диапозон от 10 до 20 чисел в векторе
+            SendMessage(hList, LB_RESETCONTENT, 0, 0); // очищаем список
+            while (arr.size() <= number) // пока размер вектора не будет равнятся диапозону будет срабатывать блок кода снизу
             {
-                arr.push_back(min2 + rand() % (max2 - min2 + 1));
-                if (arr.back() == 0) {
+                arr.push_back(min2 + rand() % (max2 - min2 + 1)); // генерируем числа
+                if (arr.back() == 0) { // если число равно 0 то удаляем его
                     arr.pop_back();
                 }
             }
-            for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++)
+            for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++) // засовываем числа в строку и отправляем List Box
             {
-                _stprintf_s(str, TEXT("%i"), *it);
-                SendMessage(hList, LB_ADDSTRING, 0, LPARAM(str));
+                _stprintf_s(str, TEXT("%d"), *it); // записываем в строку 
+                SendMessage(hList, LB_ADDSTRING, 0, LPARAM(str)); // отправляем в List Box
             }
             switch (index) {
-            case 0:
+            case 0: // произведение
                 summ = 1;
                 for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++)
                 {
                     summ *= *it;
                 }
                 break;
-            case 1:
+            case 1: // среднее арифметическое
                 for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++)
                 {
                     summ += *it;
                 }
                 summ /= number;
-            case 2:
+            case 2: // сумма всех чисел
                 for (vector<int>::iterator it = arr.begin(); it != arr.end(); it++)
                 {
                     summ += *it;
                 }
                 break;
             }
-            _stprintf_s(str2, TEXT("%.2f"), summ);
-            SetWindowText(hEdit, str2);
+            _stprintf_s(str2, TEXT("%.2f"), summ); // сумму записываем во вторую строку 
+            SetWindowText(hEdit, str2); // отправляем строку с суммой в Edit Control
         }
-        else if (wp == IDC_BUTTON2) {
+        else if (wp == IDC_BUTTON2) { // если нажали кнопку выхода то приложение закрывается
             EndDialog(hWnd, 0);
         }
     }
